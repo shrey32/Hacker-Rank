cc5abeb8d6be72b66cb5c67bcd548a85dd9b17c0
Shrey <shrey.sharma@divergentsl.com>
2014-10-26 16:21:53 +0530

    running time of quicksort compelted

--- src/com/hackerrank/arraysorting/RunningTimeOfQuickSort.java
+++ src/com/hackerrank/arraysorting/RunningTimeOfQuickSort.java
@@ -1,7 +1,5 @@
 package com.hackerrank.arraysorting;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 /**The running time of Quicksort will depend on how balanced the partitions are. If you are unlucky and select the greatest or the smallest element as the pivot, then each partition will separate only one element at a time, so the running time will be similar to InsertionSort.
@@ -52,31 +50,62 @@ import java.util.Scanner;
  */
 public class RunningTimeOfQuickSort {
 
-	public static void main(String[] args) {
+	static int swap = 0, shift = 0;
 
-		Scanner scan = new Scanner(System.in);
-
-		int N = scan.nextInt();
-		int count = 0;
-		List<Integer> arr = new ArrayList<Integer>();
-		for (int i = 0; i < N; i++) {
-			arr.add(scan.nextInt());
-		}
-
-		for (int i = 1; i < arr.size(); i++) {
-			int j = i;
-			int x = arr.get(i);
-			while (j > 0 && arr.get(j - 1) > x) {
-				int temp = arr.get(j);
-				arr.set(j, arr.get(j - 1));
-				arr.set(j - 1, temp);
-				count++;
-				j--;
+	static int partition(int a[], int start, int end) {
+		int item = a[end], p = start, temp;
+		for (int i = start; i < end; i++) {
+			if (a[i] <= item) {
+				temp = a[i];
+				a[i] = a[p];
+				a[p] = temp;
+				p++;
+				swap++;
 			}
+
+		}
+		a[end] = a[p];
+		a[p] = item;
+		swap++;
+		return p;
+	}
+
+	static void Quick(int q[], int start, int end) {
+		if (start < end) {
+			int j = partition(q, start, end);
+			Quick(q, start, j - 1);
+			Quick(q, j + 1, end);
+		}
+	}
+
+	static void insertion(int a[], int size) {
+		for (int u = 1; u < size; u++) {
+			int j = u - 1, item = a[u];
+			while (j >= 0 && a[j] > item) {
+				a[j + 1] = a[j];
+				j--;
+				shift++;
+			}
+			a[j + 1] = item;
+
+		}
+	}
+
+	public static void main(String... $) {
+		Scanner in = new Scanner(System.in);
+		int n = in.nextInt();
+		int ar[] = new int[n];
+		int ar1[] = new int[n];
+
+		for (int i = 0; i < n; i++) {
+			ar[i] = in.nextInt();
+			ar1[i] = ar[i];
 		}
 
-		System.out.println(count);
-		scan.close();
+		Quick(ar, 0, n - 1);
+		insertion(ar1, n);
 
+		System.out.println(shift - swap);
+		in.close();
 	}
 }
