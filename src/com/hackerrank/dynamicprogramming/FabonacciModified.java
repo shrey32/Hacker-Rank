c0eee9b1261b1593939f96757ad4ad34ceab17bd
Parikshit Patel <parikshit.patel@divergentsl.com>
2014-11-27 14:32:57 +0530

    hacker rank fabonacci modified added docs

--- src/com/hackerrank/dynamicprogramming/FabonacciModified.java
+++ src/com/hackerrank/dynamicprogramming/FabonacciModified.java
@@ -3,6 +3,49 @@ package com.hackerrank.dynamicprogramming;
 import java.math.BigInteger;
 import java.util.Scanner;
 
+/**Problem Statement
+
+ A series is defined in the following manner:
+
+ Given the nth and (n+1)th terms, the (n+2)th can be computed by the following relation 
+ Tn+2 = (Tn+1)2 + Tn
+
+ So, if the first two terms of the series are 0 and 1: 
+ the third term = 12 + 0 = 1 
+ fourth term = 12 + 1 = 2 
+ fifth term = 22 + 1 = 5 
+ ... And so on.
+
+ Given three integers A, B and N, such that the first two terms of the series (1st and 2nd terms) are A and B respectively, compute the Nth term of the series.
+
+ Input Format
+
+ You are given three space separated integers A, B and N on one line.
+
+ Input Constraints 
+ 0 <= A,B <= 2 
+ 3 <= N <= 20
+
+ Output Format
+
+ One integer. 
+ This integer is the Nth term of the given series when the first two terms are A and B respectively.
+
+ Sample Input 
+ 0 1 5
+
+ Sample Output 
+ 5
+
+ Explanation
+
+ The first two terms of the series are 0 and 1. The fifth term is 5. How we arrive at the fifth term, is explained step by step in the introductory sections   
+ */
+
+/**
+ * @author Shrey
+ *
+ */
 public class FabonacciModified {
 
 	public static void main(String[] args) {
@@ -13,7 +56,7 @@ public class FabonacciModified {
 		BigInteger b1 = BigInteger.valueOf(b);
 		BigInteger a1 = BigInteger.valueOf(a);
 		int i = 0;
-        BigInteger c1=new BigInteger("0");
+		BigInteger c1 = new BigInteger("0");
 		while (i < N) {
 
 			c1 = a1;
