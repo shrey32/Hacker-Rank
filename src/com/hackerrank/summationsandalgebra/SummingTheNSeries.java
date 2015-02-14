6df3738d2ae4794cbe53e3c618f581b81f1a0d1d
Shrey Sharma <shrey.sharma@divergentsl.com>
2015-02-14 16:32:11 +0530

    comment added

--- src/com/hackerrank/summationsandalgebra/SummingTheNSeries.java
+++ src/com/hackerrank/summationsandalgebra/SummingTheNSeries.java
@@ -4,6 +4,52 @@ import java.io.IOException;
 import java.math.BigInteger;
 import java.util.Scanner;
 
+/**
+ * Problem Statement
+
+ Mandarin | Russian | Japanese
+
+ You are given a series, whose nth term is defined as
+
+ Tn = n2 - (n-1)2
+
+ You have to find the Sum of the series Sn = T1 + T2 + T3 + .... + Tn
+
+ Find Sn % (109 + 7)
+
+ Input Format
+
+ First line contains an integer,T, denoting the number of testcases. 
+ Each test case contains an integer n
+
+ Output Format
+
+ For each test case, print the required sum % (109 + 7)
+
+ Constraints
+
+ 1 <= T <= 10 
+ 1 <= n <= 1016
+
+ Sample Input
+
+ 2
+ 2 
+ 1
+ Sample Output
+
+ 4
+ 1
+ Explanation 
+ Case#00 : we have 4 = 1 + 3 
+ Case#01 : we have 1 = 1
+ */
+
+/**
+ * 
+ * @author Shrey
+ *
+ */
 public class SummingTheNSeries {
 
 	public static void main(String[] args) throws NumberFormatException,
