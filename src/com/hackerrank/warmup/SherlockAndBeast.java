22db9b2165fec2625c190f56919c05d54dde68c3
shrey32 <shreysharma.sharma31@gmail.com>
2015-12-03 14:06:16 +0530

    Sherlock And The Beast Solution

--- src/com/hackerrank/warmup/SherlockAndBeast.java
+++ src/com/hackerrank/warmup/SherlockAndBeast.java
@@ -2,28 +2,82 @@ package com.hackerrank.warmup;
 
 import java.util.Scanner;
 
+/**
+ * Problem Statement
+
+ Sherlock Holmes suspects his archenemy, Professor Moriarty, is once again plotting something diabolical. Sherlock's companion, Dr. Watson, suggests Moriarty may be responsible for MI6's recent issues with their supercomputer, The Beast.
+
+ Shortly after resolving to investigate, Sherlock receives a note from Moriarty boasting about infecting The Beast with a virus; however, he also gives him a clue—a number, N. Sherlock determines the key to removing the virus is to find the largest Decent Number having N digits.
+
+ A Decent Number has the following properties:
+
+ Its digits can only be 3's and/or 5's.
+ The number of 3's it contains is divisible by 5.
+ The number of 5's it contains is divisible by 3.
+ Moriarty's virus shows a clock counting down to The Beast's destruction, and time is running out fast. Your task is to help Sherlock find the key before The Beast is destroyed!
+
+ Constraints
+ 1≤T≤20
+ 1≤N≤100000
+
+
+ Input Format
+
+ The first line is an integer, T, denoting the number of test cases.
+
+ The T subsequent lines each contain an integer, N, detailing the number of digits in the number.
+
+ Output Format
+
+ Print the largest Decent Number having N digits; if no such number exists, tell Sherlock by printing -1.
+
+ Sample Input
+
+ 4
+ 1
+ 3
+ 5
+ 11
+ Sample Output
+
+ -1
+ 555
+ 33333
+ 55555533333
+ Explanation
+
+ For N=1, there is no such number.
+ For N=3, 555 is the only possible number.
+ For N=5, 33333 is the only possible number.
+ For N=11, 55555533333 and all permutations of these digits are valid numbers; among them, the given number is the largest one.
+ */
+/**
+ * 
+ * @author Shrey
+ *
+ */
 public class SherlockAndBeast {
 
-	 public static void main(String[] args) {
-		         Scanner in = new Scanner(System.in);
-		         int t = in.nextInt();
-		         for(int i =0;i<t;i++){
-		             int n = in.nextInt();
-		             StringBuffer answer = new StringBuffer();
-		             for(int j = 0;j <= n/5;j++){
-		                 if((n-5*j)%3 == 0){
-		                     for(int k = 0;k< n-5*j;k++)
-		                         answer.append("5");
-		                     for(int k = 0;k < 5*j;k++)
-		                         answer.append("3");
-		                     System.out.println(answer.toString());
-		                     break;
-		                 }
-		           }
-		             if(answer.toString().equals(""))
-		                 System.out.println(-1);
-		         }  
-		         in.close();
-		  }
-	
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int t = in.nextInt();
+		for (int i = 0; i < t; i++) {
+			int n = in.nextInt();
+			StringBuffer answer = new StringBuffer();
+			for (int j = 0; j <= n / 5; j++) {
+				if ((n - 5 * j) % 3 == 0) {
+					for (int k = 0; k < n - 5 * j; k++)
+						answer.append("5");
+					for (int k = 0; k < 5 * j; k++)
+						answer.append("3");
+					System.out.println(answer.toString());
+					break;
+				}
+			}
+			if (answer.toString().equals(""))
+				System.out.println(-1);
+		}
+		in.close();
+	}
+
 }
