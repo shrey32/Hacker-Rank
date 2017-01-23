904589a50a502a97b297a823577fff9d2d6497ea
shrey32 <shreysharma.sharma31@gmail.com>
2017-01-23 15:09:32 +0530

    Is This a Binary Search Tree? solution

--- src/com/hackerrank/datastructures/tree/BinarySearchTreeInsertion.java
+++ src/com/hackerrank/datastructures/tree/BinarySearchTreeInsertion.java
@@ -21,19 +21,20 @@ public class BinarySearchTreeInsertion {
 			node = Insert(node, Integer.valueOf(s));
 		}
 		n = Integer.parseInt(br.readLine());
-		node=Insert(node, n);
+		node = Insert(node, n);
 		System.out.println(node);
 	}
 
 	private static class Node {
 		public int data;
 		public Node left, right;
+
 		@Override
 		public String toString() {
 			return "Node [data=" + data + ", left=" + left + ", right=" + right
 					+ "]";
 		}
-        
+
 	}
 
 	private static Node Insert(Node root, int value) {
@@ -52,5 +53,18 @@ public class BinarySearchTreeInsertion {
 			root.data = value;
 		}
 		return root;
+	}
+
+	boolean checkBST(Node root) {
+		return checkBST(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+	}
+
+	boolean checkBST(Node root, int min, int max) {
+		if (root == null)
+			return true;
+		if (root.data < min || root.data > max)
+			return false;
+		return (checkBST(root.left, min, root.data - 1) && checkBST(root.right,
+				root.data + 1, max));
 	}
 }
