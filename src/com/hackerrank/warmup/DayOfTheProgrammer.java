cdef8b4ea710e4492d8f23f6b51c5341a5450170
Shrey <shreysharma.sharma31@gmail.com>
2017-08-01 08:40:33 +0530

    Day Of The Programmer solved

---
+++ src/com/hackerrank/warmup/DayOfTheProgrammer.java
@@ -0,0 +1,70 @@
+package com.hackerrank.warmup;
+
+import java.util.Scanner;
+
+/**
+ *Marie invented a Time Machine and wants to test it by time-traveling to visit Russia on the Day of the Programmer (the  day of the year) during a year in the inclusive range from  to .
+
+ From  to , Russia's official calendar was the Julian calendar; since  they used the Gregorian calendar system. The transition from the Julian to Gregorian calendar system occurred in , when the next day after January  was February . This means that in , February  was the  day of the year in Russia.
+
+ In both calendar systems, February is the only month with a variable amount of days; it has  days during a leap year, and  days during all other years. In the Julian calendar, leap years are divisible by ; in the Gregorian calendar, leap years are either of the following:
+
+ Divisible by .
+ Divisible by  and not divisible by .
+ Given a year, , find the date of the  day of that year according to the official Russian calendar during that year. Then print it in the format dd.mm.yyyy, where dd is the two-digit day, mm is the two-digit month, and yyyy is .
+
+ Input Format
+
+ A single integer denoting year .
+
+ Constraints
+
+ Output Format
+
+ Print the full date of Day of the Programmer during year  in the format dd.mm.yyyy, where dd is the two-digit day, mm is the two-digit month, and yyyy is .
+
+ Sample Input 0
+
+ 2017
+ Sample Output 0
+
+ 13.09.2017
+ Explanation 0
+
+ In the year , January has  days, February has  days, March has  days, April has  days, May has days, June has  days, July has  days, and August has  days. When we sum the total number of days in the first eight months, we get . Day of the Programmer is the day, so then calculate  to determine that it falls on day  of the  month (September). We then print the full date in the specified format, which is 13.09.2017.
+
+ Sample Input 1
+
+ 2016
+ Sample Output 1
+
+ 12.09.2016
+ Explanation 1
+
+ Year  is a leap year, so February has  days but all the other months have the same number of days as in . When we sum the total number of days in the first eight months, we get . Day of the Programmer is the  day, so then calculate  to determine that it falls on day  of the  month (September). We then print the full date in the specified format, which is 12.09.2016.
+ *
+ */
+/**
+ * 
+ * @author Shrey
+ *
+ */
+public class DayOfTheProgrammer {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int year = in.nextInt();
+		in.close();
+		String result = "";
+		if (year == 1918) {
+			result = "26.09.1918";
+		} else if ((year <= 1917)
+				& (year % 4 == 0)
+				|| ((year > 1918) & (year % 400 == 0 || ((year % 4 == 0) && (year % 100 != 0))))) {
+			result = "12.09." + year;
+		} else {
+			result = "13.09." + year;
+		}
+		System.out.println(result);
+	}
+}
