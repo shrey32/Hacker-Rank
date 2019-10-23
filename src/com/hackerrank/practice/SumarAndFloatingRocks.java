67818fbc1d68ca66d35d0b95f80772416328b6ef
Shrey <shreysharma.sharma31@gmail.com>
2019-10-24 02:32:47 +0530

    Sumar And Floating Rocks

---
+++ src/com/hackerrank/practice/SumarAndFloatingRocks.java
@@ -0,0 +1,28 @@
+package com.hackerrank.practice;
+
+public class SumarAndFloatingRocks {
+
+	public static void main(String[] args) {
+		System.out.println(countPoints(0, 2, 4, 0));
+		System.out.println(countPoints(2, 2, 5, 5));
+		System.out.println(countPoints(1, 9, 8, 16));
+	}
+
+	static int countPoints(int x1, int y1, int x2, int y2) {
+
+		if (x1 == x2)
+			return Math.abs(y2 - y1) - 1;
+
+		if (y1 == y2)
+			return Math.abs(x2 - x1) - 1;
+
+		return gcd(Math.abs(y2 - y1), Math.abs(x2 - x1)) - 1;
+	}
+
+	static int gcd(int a, int b) {
+		if (b == 0)
+			return a;
+		return gcd(b, a % b);
+	}
+
+}
