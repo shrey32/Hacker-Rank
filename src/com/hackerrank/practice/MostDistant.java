3c92819323e56d54dda6b8467a6546961fb98286
Shrey <shreysharma.sharma31@gmail.com>
2019-10-25 00:31:30 +0530

    Adding question infor

--- src/com/hackerrank/practice/MostDistant.java
+++ src/com/hackerrank/practice/MostDistant.java
@@ -1,5 +1,44 @@
 package com.hackerrank.practice;
 
+/**
+ * Keko has  dots in a 2-D coordinate plane. He wants to measure the gap between the most distant two dots. To make the problem easier, Keko decided to change each dot's  or  coordinate to zero.
+
+Help Keko calculate the distance!
+
+Input Format
+
+The first line contains an integer, , the number of dots.
+The next  lines each contain the integer coordinates of the dots in  fashion.
+
+Constraints
+
+
+It is guaranteed that all dots are distinct, and either their  or  coordinate is equal to .
+
+Output Format
+
+Print the distance between the most distant dots with an absolute error of, at most, .
+
+Sample Input
+
+4
+-1 0
+1 0
+0 1
+0 -1
+Sample Output
+
+2.000000
+Explanation
+
+In the sample, the most distant dots are located at  and .
+The distance between them is .
+ */
+/**
+ * 
+ * @author Shrey
+ *
+ */
 public class MostDistant {
 
 	public static void main(String[] args) {
