0656addd4b4d05669dda8c17086bec55357b7095
Shrey <shreysharma.sharma31@gmail.com>
2019-11-03 02:38:00 +0530

    Gordon and Gallery Java

---
+++ src/com/hackerrank/practice/GordonAndGallery.java
@@ -0,0 +1,20 @@
+package com.hackerrank.practice;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class GordonAndGallery {
+
+	public static void main (String[] args) throws java.lang.Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int t = Integer.parseInt(br.readLine());
+		while(t-->0){
+			int n = Integer.parseInt(br.readLine());
+			
+			System.out.println(n/3);
+		}
+		br.close();
+	}
+
+}
