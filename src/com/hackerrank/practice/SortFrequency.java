ac3b0adf25ca263bf79520bcf7c48acb4b932b1d
Shrey <shreysharma.sharma31@gmail.com>
2019-12-06 00:59:10 +0530

    SortFrequency Java

---
+++ src/com/hackerrank/practice/SortFrequency.java
@@ -0,0 +1,32 @@
+package com.hackerrank.practice;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+/**
+ * 
+ * <a href="https://www.spoj.com/problems/RG11/">RG11 - SORT FREQUENCY OF
+ * INTEGER GRADO 11</a>
+ * 
+ * @author Shrey
+ *
+ */
+public class SortFrequency {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int n = scan.nextInt();
+		Map<Integer, Integer> frequencyMap = new HashMap<>();
+		while (n-- > 0) {
+			int in = scan.nextInt();
+			frequencyMap.put(in, frequencyMap.getOrDefault(in, 0) + 1);
+		}
+		System.out.println(frequencyMap.values().stream().sorted(Comparator.reverseOrder()).map(String::valueOf)
+				.collect(Collectors.joining(" ")));
+		scan.close();
+	}
+
+}
