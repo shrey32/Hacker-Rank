a5ab23b066226a62754eb1a16d79a2c9c717b653
Shrey <shreysharma.sharma31@gmail.com>
2020-04-12 14:16:38 +0530

    removing unused imports

--- src/com/leetcode/monthchallenge/LastStoneWeight.java
+++ src/com/leetcode/monthchallenge/LastStoneWeight.java
@@ -1,9 +1,6 @@
 package com.leetcode.monthchallenge;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Comparator;
-import java.util.List;
 import java.util.PriorityQueue;
 
 /**
