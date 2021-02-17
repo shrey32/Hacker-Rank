6ad60949a83ac36705f148de2af1cdf8eb9a80c0
shrey32 <shreysharma.sharma31@gmail.com>
2021-02-17 20:34:12 +0000

    Container With Most Water Brute Force approach

--- src/com/leetcode/ContainerWithMostWater.java
+++ src/com/leetcode/ContainerWithMostWater.java
@@ -8,8 +8,31 @@ package com.leetcode;
 public class ContainerWithMostWater {
 
 	public static void main(String[] args) {
-		System.out.println(maxArea(new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 }));
+		System.out.println(maxArea(new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 }, false));
+		System.out.println(maxArea(new int[] { 1, 1 }, false));
+		System.out.println(maxArea(new int[] { 4, 3, 2, 1, 4 }, false));
+		System.out.println(maxArea(new int[] { 1, 2, 1 }, false));
+	}
 
+	public static int maxArea(int[] height, boolean val) {
+		if (height == null || height.length <= 1)
+			return 0;
+		if (height.length == 2)
+			return Math.min(height[0], height[1]);
+
+		int max = 0;
+
+		for (int i = 0; i < height.length; i++) {
+			int start = height[i];
+			int count = 0;
+			for (int j = i + 1; j < height.length; j++) {
+				int next = height[j];
+				count++;
+				max = Math.max(max, count * Math.min(start, next));
+			}
+		}
+
+		return max;
 	}
 
 	public static int maxArea(int[] height) {
