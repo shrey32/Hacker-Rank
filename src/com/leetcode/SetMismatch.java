d36b900db56aef5908e6e009bb0e21f0f5f09bf6
shrey32 <shreysharma.sharma31@gmail.com>
2021-03-02 09:41:50 +0000

    Set Mismatch

---
+++ src/com/leetcode/SetMismatch.java
@@ -0,0 +1,36 @@
+package com.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * 
+ * @author Shrey
+ *
+ */
+public class SetMismatch {
+
+	public static void main(String[] args) {
+		System.out.println(Arrays.toString(findErrorNums(new int[] { 1, 2, 2, 4 })));// 2,3
+	}
+
+	public static int[] findErrorNums(int[] nums) {
+
+		int n = nums.length;
+		int totalSum = 0;
+		for (int i = 1; i <= n; i++)
+			totalSum += i;
+
+		Arrays.sort(nums);
+		int sum = nums[0];
+		int first = 0;
+		for (int i = 0; i < n - 1; i++) {
+			sum += nums[i + 1];
+			if (nums[i] == nums[i + 1]) {
+				first = nums[i];
+				sum = sum - first;
+			}
+		}
+		return new int[] { first, totalSum - sum };
+	}
+
+}
