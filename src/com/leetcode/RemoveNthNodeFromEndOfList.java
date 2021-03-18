10b232401d339145d306c321f2edb7ba7eee6ef9
shrey32 <shreysharma.sharma31@gmail.com>
2021-03-18 18:50:42 +0000

    Remove Nth Node from the End of the List Java

---
+++ src/com/leetcode/RemoveNthNodeFromEndOfList.java
@@ -0,0 +1,52 @@
+package com.leetcode;
+
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ * 
+ * @author Shrey
+ *
+ */
+public class RemoveNthNodeFromEndOfList {
+
+	public static void main(String[] args) {
+
+	}
+
+	public static ListNode removeNthFromEnd(ListNode head, int n) {
+		if (head == null)
+			return null;
+		List<ListNode> list = new ArrayList<>();
+		ListNode temp = head;
+		while (temp != null) {
+			list.add(temp);
+			temp = temp.next;
+		}
+
+		int k = list.size() - 1 - n;
+
+		if (list.size() == 1) {
+			return null;
+		} else if (list.size() == n) {
+			head = head.next;
+			list.get(0).next = null;
+		} else if (n == 1) {
+			list.get(k).next = null;
+		} else {
+			list.get(k).next = list.get(k + 2);
+		}
+		return head;
+	}
+
+	private static class ListNode {
+		private int val;
+
+		ListNode next;
+
+		public ListNode(int x) {
+			val = x;
+			next = null;
+		}
+	}
+}
