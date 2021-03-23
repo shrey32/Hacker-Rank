27da50df76fc486b0e3323b96f8615bf22f9ffde
shrey32 <shreysharma.sharma31@gmail.com>
2021-03-23 14:20:56 +0000

    3 Sum With Multiplicity

---
+++ src/com/leetcode/ThreeSumWithMultiplicity.java
@@ -0,0 +1,43 @@
+package com.leetcode;
+
+import java.util.Map;
+import java.util.List;
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * 
+ * @author Shrey
+ *
+ */
+public class ThreeSumWithMultiplicity {
+
+	public static void main(String[] args) {
+		System.out.println(threeSumMulti(new int[] { 1, 1, 2, 2, 3, 3, 4, 4, 5, 5 }, 8));
+	}
+
+	public static int threeSumMulti(int[] arr, int target) {
+		int mod = 1000000007;
+		Map<Integer, Long> map = new HashMap<>();
+		for (int i : arr)
+			map.put(i, map.getOrDefault(i, 0l) + 1l);
+		long result = 0;
+		for (int key1 : map.keySet()) {
+			for (int key2 : map.keySet()) {
+				int diff = target - key1 - key2;
+				if (map.containsKey(diff)) {
+					if (key1 == key2 && key1 == diff) {
+						result += ((map.get(key1)) * (map.get(key1) - 1) * (map.get(key1) - 2)) / 6;
+					} else if (key1 == key2 && key1 != diff) {
+						result += ((map.get(key1) * (map.get(key1) - 1)) / 2 * map.get(diff));
+					} else if (key1 < key2 && key2 < diff) {
+						result += ((map.get(key1) * map.get(key2) * map.get(diff)));
+					}
+				}
+			}
+		}
+		return ((int) (result % mod));
+	}
+
+}
