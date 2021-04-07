3349624809358a87ad254c412dd4e70ced971f2e
shrey32 <shreysharma.sharma31@gmail.com>
2021-04-07 18:14:48 +0100

    Adding O(n) approach

--- src/com/hackerrank/crackingcodinginterview/StringOneOrZeroEditAway.java
+++ src/com/hackerrank/crackingcodinginterview/StringOneOrZeroEditAway.java
@@ -8,9 +8,45 @@ package com.hackerrank.crackingcodinginterview;
 public class StringOneOrZeroEditAway {
 
 	public static void main(String[] args) {
+		System.out.println("O(m*n) approach using DP");
 		System.out.println(minEditDistance("pale", "ple"));
 		System.out.println(minEditDistance("pale", "bake"));
 		System.out.println(minEditDistance("pale", "pale"));
+		System.out.println("O(n) approach");
+		System.out.println(isEditDistanceOneOrZero("pale", "ple"));
+		System.out.println(isEditDistanceOneOrZero("pale", "bake"));
+		System.out.println(isEditDistanceOneOrZero("pale", "pale"));
+	}
+
+	public static boolean isEditDistanceOneOrZero(String s1, String s2) {
+		if (s1.equals(s2))
+			return true;
+		int m = s1.length();
+		int n = s2.length();
+		// diff should be less than or equal to 1
+		if (Math.abs(m - n) > 1)
+			return false;
+		int i = 0, j = 0;
+		int count = 0;
+		while (i < m && j < n) {
+			if (s1.charAt(i) != s2.charAt(j)) {
+				if (count == 1)
+					return false;
+				if (m > n)
+					i++;
+				else if (m < n)
+					j++;
+				else {
+					i++;
+					j++;
+				}
+				count++;
+			} else {
+				i++;
+				j++;
+			}
+		}
+		return count <= 1;
 	}
 
 	public static boolean minEditDistance(String s1, String s2) {
