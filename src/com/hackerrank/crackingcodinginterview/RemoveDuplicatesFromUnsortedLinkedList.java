6c3db7dc997d1c8894723d5d1c6816e32c181dc3
shrey32 <shreysharma.sharma31@gmail.com>
2021-04-09 17:34:58 +0100

    Linked List problems

---
+++ src/com/hackerrank/crackingcodinginterview/RemoveDuplicatesFromUnsortedLinkedList.java
@@ -0,0 +1,71 @@
+package com.hackerrank.crackingcodinginterview;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class RemoveDuplicatesFromUnsortedLinkedList {
+
+	public static void main(String[] args) {
+		System.out.println(removeDuplicates(input1()));
+		System.out.println(removeDuplicates(input2()));
+	}
+
+	public static ListNode removeDuplicates(ListNode head) {
+		ListNode temp = head;
+		Set<Integer> entries = new HashSet<>();
+		ListNode prev = null;
+		while (temp != null) {
+			if (entries.contains(temp.val)) {
+				if (temp.next != null) {
+					temp.val = temp.next.val;
+					temp.next = temp.next.next;
+				} else if (prev != null) {
+					prev.next = null;
+				}
+			} else {
+				entries.add(temp.val);
+			}
+			prev = temp;
+			temp = temp.next;
+		}
+		return head;
+	}
+
+	private static ListNode input1() {
+		ListNode head = new ListNode(1);
+		head.next = new ListNode(2);
+		head.next.next = new ListNode(3);
+		head.next.next.next = new ListNode(2);
+		head.next.next.next.next = new ListNode(5);
+		head.next.next.next.next.next = new ListNode(4);
+		head.next.next.next.next.next.next = new ListNode(4);
+		return head;
+	}
+
+	private static ListNode input2() {
+		ListNode head = new ListNode(1);
+		head.next = new ListNode(2);
+		return head;
+	}
+
+	private static class ListNode {
+		private int val;
+		ListNode next;
+
+		ListNode(int x) {
+			this.val = x;
+		}
+
+		@Override
+		public String toString() {
+			ListNode temp = this;
+			StringBuilder sb = new StringBuilder();
+			while (temp != null) {
+				sb.append(temp.val + " ");
+				temp = temp.next;
+			}
+			return sb.toString();
+		}
+	}
+
+}
