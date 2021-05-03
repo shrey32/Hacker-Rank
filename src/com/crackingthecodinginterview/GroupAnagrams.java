7e3539c00b053fcd5e901d2693f6609b81f52157
shrey32 <shreysharma.sharma31@gmail.com>
2021-05-03 16:08:59 +0100

    Grouping anagrams

---
+++ src/com/crackingthecodinginterview/GroupAnagrams.java
@@ -0,0 +1,30 @@
+package com.crackingthecodinginterview;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+/**
+ * 
+ * @author Shrey
+ *
+ */
+public class GroupAnagrams {
+
+	public static void main(String[] args) {
+		System.out.println(groupAnagrams(new String[] { "eat", "tea", "tan", "ate", "nat", "bat" }));
+	}
+
+	public static List<List<String>> groupAnagrams(String[] strs) {
+		return new ArrayList<>(
+				Arrays.stream(strs).collect(Collectors.groupingBy(s -> getKey(s), Collectors.toList())).values());
+	}
+
+	public static String getKey(String s) {
+		char[] c = s.toCharArray();
+		Arrays.sort(c);
+		return new String(c);
+	}
+
+}
