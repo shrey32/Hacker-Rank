bece9e0f8a30e43a0b49b674b1ee9db401fc44ce
shrey32 <shreysharma.sharma31@gmail.com>
2021-05-16 20:27:59 +0100

    Binary Tree Cameras Java

---
+++ src/com/leetcode/BinaryTreeCameras.java
@@ -0,0 +1,32 @@
+package com.leetcode;
+
+/**
+ * 
+ * @author Shrey
+ *
+ */
+public class BinaryTreeCameras {
+
+	private int ans = 0;
+
+	public int minCameraCover(TreeNode root) {
+		return dfs(root) > 2 ? ans + 1 : ans;
+	}
+
+	public int dfs(TreeNode node) {
+		if (node == null)
+			return 0;
+		int val = dfs(node.left) + dfs(node.right);
+		if (val == 0)
+			return 3;
+		if (val < 3)
+			return 0;
+		ans++;
+		return 1;
+	}
+
+	private static class TreeNode {
+		TreeNode left, right;
+	}
+
+}
