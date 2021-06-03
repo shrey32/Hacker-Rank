3522473f562e40e1ccb139f7cc5cf57208602e01
shrey32 <shreysharma.sharma31@gmail.com>
2021-06-03 21:21:09 +0100

    Maximum Area of a Piece of Cake After Horizontal and Vertical Cuts

---
+++ src/com/leetcode/MaximumAreaOfAPieceOfCakeAfterHorizontalAndVerticalCuts.java
@@ -0,0 +1,27 @@
+package com.leetcode;
+
+import java.util.Arrays;
+
+/**
+ * 
+ * @author Shrey
+ *
+ */
+public class MaximumAreaOfAPieceOfCakeAfterHorizontalAndVerticalCuts {
+
+	public static void main(String[] args) {
+		System.out.println(maxArea(5, 4, new int[] {}, new int[] {}));
+	}
+
+	public static int maxArea(int h, int w, int[] hc, int[] vc) {
+		Arrays.sort(hc);
+		Arrays.sort(vc);
+		int maxh = Math.max(hc[0], h - hc[hc.length - 1]), maxv = Math.max(vc[0], w - vc[vc.length - 1]);
+		for (int i = 1; i < hc.length; i++)
+			maxh = Math.max(maxh, hc[i] - hc[i - 1]);
+		for (int i = 1; i < vc.length; i++)
+			maxv = Math.max(maxv, vc[i] - vc[i - 1]);
+		return (int) ((long) maxh * maxv % 1000000007);
+	}
+
+}
